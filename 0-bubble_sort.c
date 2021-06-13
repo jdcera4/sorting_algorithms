9e880f286dfdc2028fe30a06f3c747fe9d85ef42
jdcera4 <jdcera4@gmail.com>
2021-06-13 14:47:32 -0500

    {} llaves

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,7 @@ void swap(int *major, int *minor)
 	*major = *minor;
 	*minor = tmp;
 }
+
 /**
  * bubble_sort - Function that sorts an array of integers in
  * ascending order(Bubble sort)
@@ -25,6 +26,9 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - i - 1; j++)
 			if (array[j] > array[j + 1])
+			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
+			}
+				
 }
