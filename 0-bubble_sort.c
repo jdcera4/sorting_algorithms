c97e0c3da1403bc002b9ef94754a2f206000f2cb
jdcera4 <jdcera4@gmail.com>
2021-06-13 17:49:29 -0500

    if array is null

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,6 @@
 #include "sort.h"
 
+
 /**
  * swap - Function that swaps two numbers in an array
  * @major: previous position
@@ -23,6 +24,11 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if(!array)
+	{
+		return
+	}
+
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - i - 1; j++)
 			if (array[j] > array[j + 1])
@@ -30,5 +36,4 @@ void bubble_sort(int *array, size_t size)
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
-				
 }
