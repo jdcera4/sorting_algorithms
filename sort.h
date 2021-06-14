e4520f07057a060d78db4656db7461fe90cb763d
jdcera4 <jdcera4@gmail.com>
2021-06-14 15:57:36 -0500

    sort h

--- sort.h
+++ sort.h
@@ -25,5 +25,8 @@ void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void quick_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void iterator(int *array, int index_lowest, int index_highest);
+int partition(int *array, int index_lowest, int index_highest);
 
 #endif
