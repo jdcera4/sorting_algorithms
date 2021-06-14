343c86525748b286c6f0fc4deebb209706883e5b
jdcera4 <jdcera4@gmail.com>
2021-06-14 15:56:48 -0500

    sort quick

---
+++ 3-quick_sort.c
@@ -0,0 +1,58 @@
+#include "sort.h"
+
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL)
+    {
+        return;
+    }
+
+    iterator(array, 0, size -1);
+}
+
+void iterator(int *array, int index_lowest, int index_highest)
+{
+    int position;
+
+    if (index_lowest < index_highest)
+    {
+        position = partition(array, index_lowest, index_highest);
+        iterator(array, index_lowest, position - 1);
+        iterator(array, position + 1, index_highest);
+    }
+}
+
+int partition(int *array, int index_lowest, int index_highest)
+{
+    int index, j, position, temp;
+    static int size, k;
+
+    if (k == 0)
+        size = index_highest + 1, k++;
+
+    position = array[index_highest];
+    index = index_lowest;
+
+    for (j = index_lowest; j < index_highest; j++)
+    {
+        if (array[j] <= position)
+        {
+            if (index != j)
+            {
+                temp = array[index];
+                array[index] = array[j];
+                array[j] = temp;
+                print_array(array, size);
+            }
+            index++;
+        }
+    }
+    if (index != index_highest)
+    {
+        temp = array[index];
+        array[index] = array[index_highest];
+        array[index_highest] = temp;
+        print_array(array, size);
+    }
+    return (index);
+}
