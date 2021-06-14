a3f50e2f256adf234da07ab7589a49ecb77271d4
Andrecast <acastrillonpuerta@gmail.com>
2021-06-14 14:13:58 -0500

    Another way

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,62 +1,45 @@
 #include "sort.h"
- 
-/*function to insert a new listint_t in sorted way in
- a sorted doubly linked list
-*/
 
-void sortedInsert(listint_t** head_ref, listint_t* newlistint_t)
+/**
+ * insertion_sort_list - Function that sorts an array of
+ * integers in ascending order
+ *@list: Double linked list
+ */
+
+void insertion_sort_list(listint_t **list)
 {
-    listint_t* current;
- 
-    // if list is empty
-    if (*head_ref == NULL)
-        *head_ref = newlistint_t;
- 
-    // if the listint_t is to be inserted at the beginning
-    // of the doubly linked list
-    else if ((*head_ref)->n >= newlistint_t->n) {
-        newlistint_t->next = *head_ref;
-        newlistint_t->next->prev = newlistint_t;
-        *head_ref = newlistint_t;
-    }
- 
-    else {
-        current = *head_ref;
- 
-        // locate the listint_t after which the new listint_t
-        // is to be inserted
-        while (current->next != NULL &&
-               current->next->n < newlistint_t->n)
-            current = current->next;
- 
-        /*Make the appropriate links */
- 
-        newlistint_t->next = current->next;
- 
-        // if the new listint_t is not inserted
-        // at the end of the list
-        if (current->next != NULL)
-            newlistint_t->next->prev = newlistint_t;
- 
-        current->next = newlistint_t;
-        newlistint_t->prev = current;
-    }
-}
- 
-// function to sort a doubly linked list using insertion sort
-void insertionSort(listint_t** head_ref)
-{
-    listint_t* sorted = NULL;
- 
-    listint_t* current = *head_ref;
-    while (current != NULL) {
-        listint_t* next = current->next;
-        current->prev = current->next = NULL;
-        sortedInsert(&sorted, current);
-        current = next;
-    }
- 
-    /*Update head_ref to point to sorted doubly linked list
-    *head_ref = sorted;
-    */
+	listint_t *tmp, *aux;
+	int control = 1;
+
+	if (list == NULL || (*list)->next == NULL)
+		return;
+
+	tmp = (*list)->next;
+
+	while (tmp)
+	{
+		control = 1;
+		if (tmp->n < tmp->prev->n)
+		{
+			aux = tmp;
+			control = 0;
+			tmp = tmp->next;
+			while (aux->prev != NULL && aux->n < aux->prev->n)
+			{
+				(aux->prev)->next = aux->next;
+				if (aux->next != NULL)
+					aux->next->prev = aux->prev;
+				aux->next =  aux->prev;
+				aux->prev = aux->next->prev;
+				if (aux->prev != NULL)
+					aux->prev->next = aux;
+				aux->next->prev = aux;
+				if (aux->prev == NULL)
+					*list = aux;
+				print_list(*list);
+			}
+		}
+		if (control)
+			tmp = tmp->next;
+	}
 }
--- sort.h
+++ sort.h
@@ -22,7 +22,8 @@ typedef struct listint_s
 void swap(int *major, int *minor, int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void print_list(const listint_t *list);
+void quick_sort(int *array, size_t size);
 
 #endif
